14a0246b88e49e632165caebe6c0a85247691beb
Vũ Hoàng Đức <47014053+nghiate142@users.noreply.github.com>
2024-01-05 22:47:48 +0700

    update

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -5,27 +5,18 @@ const jwt = require('jsonwebtoken');
 
 const loggedInUsers = {};
 
-// Hàm đăng nhập
 async function login(req, res) {
     const data = req.body;
     try {
-        // Thực hiện kiểm tra tài khoản ở userService
         const user = await userService.login(data);
-
-        // Tạo access token với thời hạn 30 giây
-        const accessToken = jwt.sign({ userId: user.id }, 'your-secret-key', { expiresIn: '30s' });
-
-        // Lưu token của người dùng vào danh sách đăng nhập
+        const accessToken = jwt.sign({ userId: user.id }, 'your-secret-key', { expiresIn: '24h' });
         loggedInUsers[user.id] = accessToken;
-
-        // Trả về token và thông báo thành công
         res.status(200).json({
             message: 'Login success',
             status: 200,
             token: accessToken,
         });
     } catch (error) {
-        // Xử lý lỗi khi đăng nhập không thành công
         res.status(404).json({
             message: 'User or password not found',
             status: 404,
@@ -33,13 +24,10 @@ async function login(req, res) {
     }
 }
 
-// Hàm đăng xuất
 function logout(userId) {
-    // Xóa token của người dùng khi đăng xuất
     delete loggedInUsers[userId];
 }
 
-// Middleware kiểm tra token trước mỗi request
 function checkToken(req, res, next) {
     const token = req.headers.authorization?.split(' ')[1];
 

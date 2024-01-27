38d794cf205af539d74e2bf56478000e73bea564
Vũ Hoàng Đức <47014053+nghiate142@users.noreply.github.com>
2024-01-28 01:30:25 +0700

    update

--- controller/audio.controller.js
+++ controller/audio.controller.js
@@ -1,10 +1,27 @@
-
+const fileService = require("../service/file.service");
 class AudioController {
     async uploadAudio(req, res) {
-        const link = req.file;
+        const link = req.file.path;
+        const { title } = req.body;
+        const data = {
+            link: link,
+            title: title
+        };
+        const newAudio = await fileService.uploadAudio(data);
+        res.json({ success: true, data: newAudio });
+    }
+
+    async getUrlAudio(req, res) {
+        const id = req.params.id
         const url = process.env.URL
-        const path = `${url}${link.path}`
-        res.json({ success: true, data: link.path });
+        const image = await fileService.getUrlAudio(id)
+        const imageUrl = `${url}${image.link}`;
+        return res.status(200).json({ success: true, data: imageUrl });
+    }
+
+    async getAll(req, res) {
+        const data = await fileService.getAllAudio()
+        res.status(200).json({ success: true, data: data });
     }
 }
 
